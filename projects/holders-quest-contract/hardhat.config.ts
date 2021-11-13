61768a57eabd1069a5a1b2f8931b91ff016b9700
Rick Love <rick@toldpro.com>
2021-11-13 16:36:57 -0600

    Merge commit '42e742fb2d42995ecc825c4d16f1b5486f376bc3'

--- projects/holders-quest-contract/hardhat.config.ts
+++ projects/holders-quest-contract/hardhat.config.ts
@@ -1,17 +1,15 @@
-import * as dotenv from "dotenv";
-
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
+import dotenv from 'dotenv';
+import { HardhatUserConfig, task } from 'hardhat/config';
 dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task(`accounts`, `Prints the list of accounts`, async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -19,24 +17,40 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+const ALCHEMY_KEY = process.env.ALCHEMI_API_KEY as string;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY as string;
+const chainIds = {
+  goerli: 5,
+  hardhat: 31337,
+  kovan: 42,
+  mainnet: 1,
+  rinkeby: 4,
+  ropsten: 3,
+} as const;
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
-  networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+  // Your type-safe config goes here
+  solidity: {
+    version: `0.8.7`,
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
     },
   },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+  defaultNetwork: `hardhat`,
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      chainId: chainIds.rinkeby,
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [RINKEBY_PRIVATE_KEY],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
   },
 };
 
--- projects/holders-quest-contract/scripts/deploy.ts
+++ projects/holders-quest-contract/scripts/deploy.ts
@@ -1,9 +1,10 @@
+/* eslint-disable no-process-exit */
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import hre from 'hardhat';
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,17 +15,21 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const NftContract = await hre.ethers.getContractFactory(`NftContract`);
+  const contract = await NftContract.deploy();
 
-  await greeter.deployed();
+  await contract.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log(`NftContract deployed to:`, {
+    address: contract.address,
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
