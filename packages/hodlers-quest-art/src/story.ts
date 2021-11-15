cb5f4ec0b0a95842d7a8c0deacb1c5a4e472b402
Rick Love <rick@toldpro.com>
2021-11-15 07:23:40 -0600

    Simplify story to first action

--- packages/hodlers-quest-art/src/story.ts
+++ packages/hodlers-quest-art/src/story.ts
@@ -61,56 +61,60 @@ You can't see anything, but you can feel that you are lying on a cold hard surfa
             {
                 name: `search the ground`,
                 description: `You search the ground...`,
-                result: {
-                    art: { svgName: `search-ground` },
-                    gameOver: `
-As you feel around your position, you realize that there is no ground anywhere around you.
-                
-There is no way you can escape...
+//                 result: {
+//                     art: { svgName: `search-ground` },
+//                     gameOver: `
+// As you feel around your position, you realize that there is no ground anywhere around you.
 
-`,
-                },
+// There is no way you can escape...
+
+// `,
+//                 },
             },
             {
                 name: `call for help`,
                 description: `You call for help...`,
-                result: {
-                    art: { svgName: `call-for-help` },
-                    gameOver: `
-You listen for a response, but hear nothing at first. 
+//                 result: {
+//                     art: { svgName: `call-for-help` },
+//                     gameOver: `
+// You listen for a response, but hear nothing at first.
 
-Then, after a moment, you hear clicking. 
+// Then, after a moment, you hear clicking.
 
-The sound grows louder as it gets near.
-                
-Suddenly, you feel a sharp pain in your stomach. Your muscles spasm for a moment, but then you are no longer able to move...
-`,
-                },
+// The sound grows louder as it gets near.
+
+// Suddenly, you feel a sharp pain in your stomach. Your muscles spasm for a moment, but then you are no longer able to move...
+// `,
+//                 },
             },
-            { name: `listen`, description: `You carefully listen without making a sound...`, result: { gameOver: false } },
+            {
+                name: `listen`,
+                description: `You carefully listen without making a sound...`,
+                // result: { gameOver: false },
+            },
         ],
     },
-    {
-        title: `Whispers`,
-        // asciiArt: asciiArt_manArmUp,
-        description: `
+//     {
+//         title: `Whispers`,
+//         // asciiArt: asciiArt_manArmUp,
+//         description: `
 
-In the distance, you hear the slight brookling of water flowing over stones, but nothing else at first.
+// In the distance, you hear the slight brookling of water flowing over stones, but nothing else at first.
 
-Then, you hear something you did not expect, a whisper in your ear that says: 
+// Then, you hear something you did not expect, a whisper in your ear that says:
 
-"Do not move... They will see you..."`,
-        glitch: {
-            ratio: 0.03,
-            messages: [`HELP ME!`, `Who are you?`, `What are you?`, `How are you?`, `Where are you?`, `Why are you?`, `I'm cold`, `I'm alone`, `I'm afraid`],
-        },
-        inventory: [],
-        actions: [
-            { name: `remain still`, description: `You decide not moving is a good idea for now...` },
-            { name: `move away`, description: `You jerk away from the whisper...` },
-            { name: `stand up`, description: `You push yourself off the ground...` },
-        ],
-    },
+// "Do not move... They will see you..."`,
+//         glitch: {
+//             ratio: 0.03,
+//             messages: [`HELP ME!`, `Who are you?`, `What are you?`, `How are you?`, `Where are you?`, `Why are you?`, `I'm cold`, `I'm alone`, `I'm afraid`],
+//         },
+//         inventory: [],
+//         actions: [
+//             { name: `remain still`, description: `You decide not moving is a good idea for now...` },
+//             { name: `move away`, description: `You jerk away from the whisper...` },
+//             { name: `stand up`, description: `You push yourself off the ground...` },
+//         ],
+//     },
 ];
 
 export const createNftAdventure_nftTextAdventure = () => {
