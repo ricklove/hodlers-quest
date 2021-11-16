dd3ad7cb3e68b17363c6639a9f090de4fb651723
Rick Love <rick@toldpro.com>
2021-11-15 19:00:51 -0600

    Keyboard focus 2

--- projects/hodlers-quest-react/src/pages/token-host.tsx
+++ projects/hodlers-quest-react/src/pages/token-host.tsx
@@ -27,7 +27,7 @@ export const TokenHost = ({ tokenId }: { tokenId: string }) => {
         <div>
             <div id={id}
                 style={{ width: 300, height: 300 }} />
-            <input type='text' ref={inputKeyboardRef} style={{ opacity: 0, height: 0, width: 0 }} />
+            <input type='text' ref={inputKeyboardRef} style={{ opacity: 0.1, fontSize: 16, position: `fixed`, bottom: 0, right: 0 }} />
         </div>
     );
 };
