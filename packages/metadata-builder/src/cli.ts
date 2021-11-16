feb21408b20c8ee54e7dad55f4b226a02321ecf5
Rick Love <rick@toldpro.com>
2021-11-16 08:25:37 -0600

    Use a single thread

--- packages/metadata-builder/src/cli.ts
+++ packages/metadata-builder/src/cli.ts
@@ -6,21 +6,32 @@ export const run = async () => {
     const PROJECT_ID_MAX = 3;
     const PROJECT_TOKEN_COUNT = 10;
 
+    const THREADS = 1;
+
     const projectIds = [... new Array(PROJECT_ID_MAX + 1)].map((_, i) => i);
     const tokenIds = projectIds
         .flatMap(projectId => [... new Array(PROJECT_TOKEN_COUNT)]
             .map((_, i) => `${projectId * PROJECT_BUCKET_SIDE + i}`));
 
     try {
-        await createScreenshots({
-            baseUrl: `http://localhost:4201/image/0`,
-            tokenIds,
-            destDir: path.resolve(`./out/nft/images`),
-            size: {
-                width: 600,
-                height: 600,
-            },
+
+        const tokenIdsSlices = [...new Array(THREADS)].map(() => [] as typeof tokenIds);
+        tokenIds.forEach((t, i) => {
+            tokenIdsSlices[i % THREADS].push(t);
         });
+
+        for (const tokenIdsSlice of tokenIdsSlices){
+            await createScreenshots({
+                baseUrl: `http://localhost:4201/image/0`,
+                tokenIds: tokenIdsSlice,
+                destDir: path.resolve(`./out/nft/images`),
+                size: {
+                    width: 600,
+                    height: 600,
+                },
+            });
+        }
+
     } catch (err){
         console.error(err);
     }
